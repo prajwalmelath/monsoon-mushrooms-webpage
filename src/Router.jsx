33719d985df5c93f4370d62f021d7266b4a3c6ff
PrajwalMelath <47899315+PrajwalMelath@users.noreply.github.com>
2024-08-25 01:20:32 +0530

    changes

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { LandingPage }  from './containers/LandingPage'
-import { Team }  from './containers/Team' 
-import { AboutUs } from './containers/AboutUs' 
-import { Contact } from './containers/Contact' 
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LandingPage } from "./containers/LandingPage";
+import { Team } from "./containers/Team";
+import { AboutUs } from "./containers/AboutUs";
+import { Contact } from "./containers/Contact";
 
 export const MainPage = () => {
   return (
     <Router>
-    <Routes>
-      <Route path='/' element={<LandingPage/>}/>
-      <Route path='/#team' element={<Team/>}/>
-      <Route path='/#aboutUs' element={<AboutUs/>}/>
-      <Route path='/#contactUs' element={<Contact/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/#team" element={<Team />} />
+        <Route path="/#aboutUs" element={<AboutUs />} />
+        <Route path="/#contactUs" element={<Contact />} />
+      </Routes>
     </Router>
-  )
-}
-
+  );
+};
